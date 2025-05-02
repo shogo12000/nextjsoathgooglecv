2ed46bb40762af1d2f5993c98c80ddb1c5929f02
sidney <shogo12000@hotmail.com>
2025-05-02 11:19:26 -0700

    api tmdb and responsive mobile & desktop

--- src/app/(pages)/movies/page.tsx
+++ src/app/(pages)/movies/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 import { apiMovies } from "@/app/lib/moviActions";
 import Pagination from "../../components/pagination";
+import Image from "next/image";
 
 const fetchMovies = async (page: number) => {
   const resp = await apiMovies(page);
@@ -32,10 +33,12 @@ export default async function Movies({
       <ul className="grid gap-2 grid-cols-4 sm:max-w-[900px] md:grid-cols-5">
         {results.map((movie: any) => (
           <li key={movie.id} className="w-full">
-            <img
+            <Image
               src={`https://image.tmdb.org/t/p/w154${movie.poster_path}`}
               alt={movie.title}
-              className="w-full h-[80%] object-cover rounded"
+              width={254}
+              height={331}
+              className="object-cover rounded"
             />
             <p className="xs-paragraph sm:text-9xl text-center mt-1">
               {movie.title.length > 10
@@ -48,7 +51,6 @@ export default async function Movies({
 
       {/* Agora usa o componente client */}
       <Pagination currentPage={server_page} totalPages={total_pages} />
- 
     </div>
   );
 }
