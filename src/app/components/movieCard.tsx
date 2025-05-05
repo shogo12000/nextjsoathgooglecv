5a0a009cff98506e593ae187821dde1809a2dab6
sidney <shogo12000@hotmail.com>
2025-05-05 00:51:15 -0700

    create radioButton and moviePage

--- src/app/components/movieCard.tsx
+++ src/app/components/movieCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { getIdMovie } from "../lib/moviActions";
+// import { getIdMovie } from "../lib/moviActions";
 import Link from "next/link";
 import { useState } from "react";
 
